0c93e567114b6538fd6287c21b5c26d28a6cf197
Pascal Hertleif <killercup@gmail.com>
2016-07-27 10:22:10 +0200

    Make it better

--- Readme.md
+++ Readme.md
@@ -7,3 +7,8 @@
 - Blessedst is what that should have read. I fyou can't handle me at my cursedst, you don't deserve me at my cursedst
 - i really need to stop listening to drake he gets me all into my feelings and shit.
 - Stopped cats from dying of alcohol poisoning after walking over damp tavern floors and cleaning themselves
+- It is writing a tongue spoken by a checkers-playing program to shine spotlight on the weather appears above Borneo
+- A doctoral student watches a poppy on behalf of feminism.
+- Elders will worship yesterday.
+- A governess owns a shotgun or boat.
+- He finds out of a solar system.
--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ const ENGLISH: &'static [&'static str; 3] = &[
 
 fn main() {
     let mut chain = Chain::new();
-    chain.order(4);
+    chain.order(1);
 
     for file in BOTS {
         chain.feed_file(format!("data/bots/{}.txt", file));
@@ -36,7 +36,18 @@ fn main() {
 
     let mut input = String::new();
     loop {
-        writeln!(io::stdout(), "{}", chain.generate_str()).unwrap();
+        let output;
+
+        'output: loop {
+            let try = chain.generate_str().replace('"', "").replace('@', "");
+            let length = try.chars().count();
+            if length >= 24 && length <= 140 {
+                output = try;
+                break 'output;
+            }
+        }
+
+        writeln!(io::stdout(), "{}", output).unwrap();
         io::stdin().read_line(&mut input).unwrap();
     }
 }
